b126efa423061fbaa4465d672e3613ce40f984f4
Philippe Marschall <philippe.marschall@gmail.com>
2022-01-20 16:13:27 +0100

    fix streaming tests

--- ncau-java17-overthehood/src/test/java/com/netcetera/ncau/java17/overthehood/JfrStreamingTests.java
+++ ncau-java17-overthehood/src/test/java/com/netcetera/ncau/java17/overthehood/JfrStreamingTests.java
@@ -2,23 +2,51 @@ package com.netcetera.ncau.java17.overthehood;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jdk.jfr.Recording;
 import  jdk.jfr.consumer.EventStream;
 
 class JfrStreamingTests {
+  
+private Recording recording;
+
+  @BeforeEach
+  private void startRecording() throws IOException {
+    this.recording = new Recording();
+    this.recording.enable("jdk.ThreadSleep");
+    this.recording.start();
+  }
+
+  @AfterEach
+  private void stopRecording() throws IOException {
+    this.recording.close();
+  }
 
   @Test
   void streamFiles() throws IOException, InterruptedException {
     try (var eventStream = EventStream.openRepository()) {
       eventStream.setReuse(true);
-      eventStream.onEvent("org.junit.TestExecution", event -> {
-        System.out.println("test executed: ");
+      eventStream.onEvent("jdk.ThreadSleep", event -> {
+        System.out.println("thread: " + event.getThread().getJavaName() + " slept for: " + event.getDuration());
+
+        // initiate termination
+        eventStream.close();
       });
-      eventStream.onEvent("org.junit.TestPlanExecution", event -> {
-        System.out.println("plan executed: ");
-      });
+
+      // start the processing in a different thread
+      eventStream.startAsync();
+
+      // generate enough events for flush to happen
+      for (int i = 0; i < 1_000; i++) {
+        Thread.sleep(1L);
+      }
+
+      eventStream.awaitTermination();
     }
   }
+  
 
 }
