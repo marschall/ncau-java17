4475a1fabf144519c52659bc6992d8a189980437
Philippe Marschall <philippe.marschall@gmail.com>
2022-04-14 17:54:53 +0200

    bump language changes

---
+++ ncau-java17-superficial/src/test/java/com/netcetera/ncau/java17/superficial/TextBlocksTests.java
@@ -0,0 +1,52 @@
+package com.netcetera.ncau.java17.superficial;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+class TextBlocksTests {
+
+  private static final String CREATE_SEQUENCE = """
+                                                CREATE SEQUENCE demo_squence 
+                                                NO CYCLE 
+                                                """;
+  
+  private static final String SELECT_SEQUENCE_VALUES = """
+                                                       WITH RECURSIVE t(n, level_num) AS (
+                                                         SELECT NEXT VALUE FOR demo_squence AS n, 1 AS level_num
+                                                           UNION ALL
+                                                         SELECT NEXT VALUE FOR demo_squence AS n, level_num + 1 AS level_num
+                                                           FROM t
+                                                          WHERE level_num < ?)
+                                                       SELECT n
+                                                         FROM t 
+                                                       """;
+
+  @Test
+  void databaseAccess() throws SQLException {
+    try (var connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+      try (var statement = connection.createStatement()) {
+        statement.execute(CREATE_SEQUENCE);
+      }
+      List<Integer> sequenceValues = new ArrayList<>(10); 
+      try (PreparedStatement selectSequenceValues = connection.prepareStatement(SELECT_SEQUENCE_VALUES)) {
+        selectSequenceValues.setInt(1, 10);
+        try (ResultSet resultSet = selectSequenceValues.executeQuery()) {
+          while (resultSet.next()) {
+            sequenceValues.add(resultSet.getInt(1));
+          }
+        }
+      }
+      assertEquals(IntStream.rangeClosed(1, 10).boxed().toList(), sequenceValues);
+    }
+  }
+
+}
